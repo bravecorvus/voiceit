c37e360967e3c56a3a843427802aea97cfe03abf
Andrew Lee <leeas@stolaf.edu>
2018-04-19 14:30:00 -0500

    ehe

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gilgameshskytrooper/voiceit/backend/app"
+	"github.com/gilgameshskytrooper/voiceit/backend/utils"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
@@ -20,7 +21,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/login", globals.Login)
 	r.HandleFunc("/register", globals.Register)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/dist/")))
+	// r.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/dist/")))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(utils.Pwd() + "dist/")))
 	n := negroni.Classic()
 	n.UseHandler(r)
 
